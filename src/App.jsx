60922fb8ddab29c55dde53e0459b434721d87a04
nelesu <nelesu329@gmail.com>
2023-01-16 23:54:53 +0300

    Added google fonts. Change logo.  Adde h1 header animation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,115 @@
-import { useEffect } from 'react'
-import { Counter } from './features/counter/Counter'
+import { useEffect, useState } from 'react'
 
 function App() {
-    useEffect(() => {}, [])
+    const [galleryImagesPaths, setGalleryImagesPaths] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetch('http://127.0.0.1/gallery/')
+            return data.json()
+        }
+
+        fetchData()
+            .then((response) => {
+                setGalleryImagesPaths(response)
+            })
+            .catch(console.error)
+    }, [])
     return (
-        <>
-            <header>
+        <div className="bg-off-white">
+            <header className="fixed px-[2vw] py-[1.875rem] w-full">
                 <a href="/" className="contents">
                     <svg
                         id="Layer_1"
                         data-name="Layer 1"
                         xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 22.73 8.59"
-                        className="w-[8rem]"
+                        viewBox="0 0 22.73 8.62"
+                        className="w-10"
                     >
                         <path
-                            d="M2.13,16.75V8.16H3.84l2,6.08c.19.57.33,1,.41,1.27.1-.31.25-.77.46-1.38l2.06-6h1.53v8.59H9.23V9.56l-2.5,7.19h-1L3.22,9.44v7.31Z"
-                            transform="translate(-2.13 -8.16)"
+                            d="M0,8.59V0H1.71l2,6.08c.19.57.33,1,.41,1.27.1-.31.25-.77.46-1.38L6.64,0H8.17V8.56H7.1V1.4L4.6,8.59h-1L1.09,1.28V8.59Z"
+                            transform="translate(0 0.03)"
                         />
                         <path
-                            d="M12,9.37V8.16h1.06V9.37Zm0,7.38V10.53h1.06v6.22Z"
-                            transform="translate(-2.13 -8.16)"
+                            d="M10.87,8.59V2.37h1.06V8.59Z"
+                            transform="translate(0 0.03)"
                         />
                         <path
-                            d="M14.84,16.75V8.16h3.81a5.2,5.2,0,0,1,1.75.23,1.85,1.85,0,0,1,1,.82,2.41,2.41,0,0,1,.36,1.29A2.16,2.16,0,0,1,21.12,12a3,3,0,0,1-1.83.8,3,3,0,0,1,.69.43,5.83,5.83,0,0,1,.94,1.14l1.49,2.34H21L19.85,15c-.33-.51-.61-.91-.82-1.18a2.61,2.61,0,0,0-.58-.58,1.84,1.84,0,0,0-.52-.23,4.26,4.26,0,0,0-.63,0H16v3.82ZM16,12h2.44a3.6,3.6,0,0,0,1.22-.16,1.27,1.27,0,0,0,.67-.52,1.39,1.39,0,0,0,.23-.77,1.3,1.3,0,0,0-.44-1,2.09,2.09,0,0,0-1.4-.39H16Z"
-                            transform="translate(-2.13 -8.16)"
+                            d="M21.53,8.59V7.39h1.2v1.2Z"
+                            transform="translate(0 0.03)"
                         />
                         <path
-                            d="M23.66,16.75v-1.2h1.2v1.2Z"
-                            transform="translate(-2.13 -8.16)"
+                            d="M15.85,1H13.24V0h6.35V1H17V8.58H15.85Z"
+                            transform="translate(0 0.03)"
                         />
                     </svg>
                 </a>
             </header>
-            <div className="grid grid-cols-2 h-screen gap-[2.125rem]">
-                <div>
-                    <Counter />
+            <div className="grid grid-cols-2 h-screen gap-[2.125rem] bg-white">
+                <div className="grid grid-cols-2">
+                    <div className="flex flex-col">
+                        {galleryImagesPaths
+                            .filter((_, idx) => idx < 9)
+                            .map(({ url }, idx) => {
+                                return (
+                                    <a href={url} className="aspect-[2/3]">
+                                        <img src={url} alt={`${idx + 1}`} />
+                                    </a>
+                                )
+                            })}
+                    </div>
+                    <div className="flex flex-col">
+                        {galleryImagesPaths
+                            .filter((_, idx) => idx > 8)
+                            .map(({ url }, idx) => {
+                                return (
+                                    <div className="aspect-[2/3]">
+                                        <img src={url} alt={`${idx + 1}`} />
+                                    </div>
+                                )
+                            })}
+                    </div>
                 </div>
-                <div className="bg-off-white pt-[24.5vh] pr-[5vw] pb-[3.75rem] pl-[4vw]">
-                    <h1>
-                        <div className="uppercase font-header leading-header tracking-header text-header">
-                            <div>we build</div>
-                            <div>digital</div>
-                            <div className="ml-1ch">flagship</div>
-                            <div>stores</div>
+                <div className="h-screen bg-off-white sticky">
+                    <div className="absolute top-[1.875rem] right-[2vw]">
+                        <p className="font-title text-right">AWWWARDS</p>
+                        <p>E-commerce of the Year ‘17, ‘18, ‘19 & ‘21</p>
+                    </div>
+                    <div className="pt-[24.5vh] pr-[5vw] pb-[3.75rem] pl-[4vw] flex flex-col justify-between h-full">
+                        <h1 className="overflow-hidden">
+                            <div className="uppercase font-header leading-header tracking-header text-header">
+                                <div className="animate-slide-to-top">
+                                    we build
+                                </div>
+                                <div className="animate-slide-to-top">
+                                    digital
+                                </div>
+                                <div className="ml-1ch animate-slide-to-top">
+                                    flagship
+                                </div>
+                                <div className="animate-slide-to-top">
+                                    stores
+                                </div>
+                            </div>
+                        </h1>
+                        <div className="max-w-xs ml-auto">
+                            <p className="font-subheader text-[28px] leading-[1.05]">
+                                We believe our industry is blinded by numbers.
+                                While buying decisions are based on emotion.
+                            </p>
+                            <div className="mt-[1.25rem]">
+                                <a href="/about">
+                                    <span>
+                                        <div className="text-[16px] uppercase font-title leading-[1.2]">
+                                            About us
+                                        </div>
+                                    </span>
+                                </a>
+                            </div>
                         </div>
-                    </h1>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,59 @@ module.exports = {
             colors: {
                 'off-white': '#F2EFE6',
             },
+            aspectRatio: {
+                '3/4': '3/4',
+                '2/3': '2/3',
+            },
+            keyframes: ({ theme }) => ({
+                'slide-to-top': {
+                    '0%': {
+                        transform: `translateY(calc(${theme(
+                            'fontSize.header'
+                        )} * 4 * ${theme('lineHeight.header')}))`,
+                    },
+                    '25%': {
+                        transform: `translateY(calc(${theme(
+                            'fontSize.header'
+                        )} * 3 * ${theme('lineHeight.header')}))`,
+                    },
+                    '50%': {
+                        transform: `translateY(calc(${theme(
+                            'fontSize.header'
+                        )} * 2 * ${theme('lineHeight.header')}))`,
+                    },
+                    '75%': {
+                        transform: `translateY(calc(${theme(
+                            'fontSize.header'
+                        )} * 1 * ${theme('lineHeight.header')}))`,
+                    },
+                    '100%': {
+                        transform: 'translateY(0)',
+                    },
+                },
+            }),
+            animation: {
+                'slide-to-top':
+                    'slide-to-top 2s cubic-bezier(0.76, 0, 0.24, 1) 1s 1 normal both',
+            },
         },
         fontFamily: {
             header: [
                 'NeueHaasGrotesk-Roman',
+                'Helvetica',
+                'Arial',
+                'sans-serif',
+            ],
+            subheader: [
+                'RecklessNeue-Book',
+                'Marvel',
+                'Helvetica',
+                'Arial',
+                'sans-serif',
+            ],
+            title: [
+                'NeueHaasGrotesk-Medium',
+                'Lato',
                 'Helvetica',
                 'Arial',
                 'sans-serif',
